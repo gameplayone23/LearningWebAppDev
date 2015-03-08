3d44f44b76cf230a2e3ef11ce38c88ea9fe3af68
gameplayone23 <gameplayone23@free.Fr>
2015-03-08 11:08:49 +0100

    Chapter 4 - Our first interactive App - Ajout d'un test pour savoir si un comment vide est ajouté

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -6,8 +6,12 @@ var main = function (){
 		// Creation d'une nouvelle variable contenant la code html à ajouter
 		// $("<p>") permet de créer un objet de type paragraphe
 		// la fonction val permet d'obtenir le contenu d'un objet de type input box
-		var $new_comment = $("<p>").text($(".comment-input input").val());
-		$(".comments").append($new_comment);
+		var $new_comment;
+
+		if ( $(".comment-input input").val() !== "" ) {
+			 $new_comment= $("<p>").text($(".comment-input input").val());
+			 $(".comments").append($new_comment);
+		};
 	});
 };
 
