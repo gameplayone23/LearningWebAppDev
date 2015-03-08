95e1a649c43168511e00a6c5ba79dca801b18679
gameplayone23 <gameplayone23@free.Fr>
2015-03-08 11:29:02 +0100

    Chapter 4 - Our first interactive App - Ajout effet fondu lors de l'ajout + factorisation du code en utilisant une fonction

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,18 +1,28 @@
 var main = function (){
 	"use strict";
 
-	// Execution d'une fonction lors du clic sur le bouton
-	$(".comment-input button").on("click", function(event){
-		// Creation d'une nouvelle variable contenant la code html à ajouter
-		// $("<p>") permet de créer un objet de type paragraphe
-		// la fonction val permet d'obtenir le contenu d'un objet de type input box
+	var addCommentFormInputBox = function(){
 		var $new_comment;
 
 		if ( $(".comment-input input").val() !== "" ) {
-			 $new_comment= $("<p>").text($(".comment-input input").val());
-			 $(".comments").append($new_comment);
+			$new_comment= $("<p>").text($(".comment-input input").val());
+			$new_comment.hide();
+			$(".comments").append($new_comment);
+			$new_comment.fadeIn();
 			 // Remise à zéro de l'input apres ajout
 			 $(".comment-input input").val("");
+		};
+	}
+
+	// Execution d'une fonction lors du clic sur le bouton
+	$(".comment-input button").on("click", function(event){
+		addCommentFormInputBox();
+	});
+
+	// Execution d'un fonction lorsque le user fait "enter"
+	$(".comment-input input").on("keypress", function (event) {
+		if ( event.keyCode == 13) {
+			addCommentFormInputBox();
 		};
 	});
 };
